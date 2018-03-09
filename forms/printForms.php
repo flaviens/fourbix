4dfb02cc37289a53229eb0d1685d3cea01f08304
Gabriel Oliveira Martins <gabriel.oliveiragom@gmail.com>
2018-03-09 14:35:29 +0100

    Ajouter label au formulaire de login

--- forms/printForms.php
+++ forms/printForms.php
@@ -3,8 +3,14 @@
 function printLoginForm($askedpage){
     echo <<<CHAINE_DE_FIN
     <form action="index.php?todo=login&page=$askedpage" method="post">
-        <p>Login : <input type="text" name="login" placeholder="login" required></p>
-        <p>Password : <input type="password" name="password"></p>
+        <p>
+            <label for="login">Login : </label>
+            <input type="text" name="login" id="login" placeholder="login" required>
+        </p>
+        <p>
+            <label for="password">Password : </label>
+            <input type="password" name="password" id="password" placeholder="password" required>
+        </p>
         <p><input type="submit" value="Valider"</p>
     </form>
     
